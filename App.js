705ad5fc500cf41b8e7774f4c0aa2c26354dc5c6
AmirBagheri <amiramb10@gmail.com>
2025-01-04 16:22:35 +0330

    more style/output ignored v:1

--- App.js
+++ App.js
@@ -7,6 +7,10 @@ const weatherIcon = document.getElementById("weatherIcon");
 let feels = document.getElementById("feels");
 let humidity = document.getElementById("humidity");
 let wind = document.getElementById("wind");
+let minWeather=document.querySelector('#min-weather')
+let maxWeather=document.querySelector('#max-weather')
+let temp=document.querySelector('#temp')
+
 
 //changes on selected city
 form.addEventListener("submit", (event) => {
@@ -20,13 +24,15 @@ async function getweatherData(city) {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`
     );
     const data = await responseData.json();
-    console.log(data);
     //using data in app
     city.value = "";
     cityResult.textContent = data.name;
     weatherType.textContent = data.weather[0].description;
     weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    feels.textContent = data.main.feels_like;
+    temp.textContent=data.main.temp+'°'+'c'
+    feels.textContent = data.main.feels_like+'°'+'c'
+    maxWeather.textContent=data.main.temp_max
+    minWeather.textContent=data.main.temp_min
     humidity.textContent = data.main.humidity + "%";
     wind.textContent = data.wind.speed + "KM/H";
   } catch (error) {
